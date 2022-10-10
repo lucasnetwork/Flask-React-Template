d44f1a4bef36457f1a10e308aab012aaafd594f3
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-10 17:46:46 -0300

    add new colors in theme

--- templates/static/src/theme/globaStyles.js
+++ templates/static/src/theme/globaStyles.js
@@ -5,6 +5,8 @@ export default createGlobalStyle`
         margin:0;
         padding:0;
         box-sizing: border-box;
+        border: none;
+        outline: none;
     }
 
     body,#app{
--- templates/static/src/theme/theme.js
+++ templates/static/src/theme/theme.js
@@ -3,5 +3,7 @@ export default {
     green: "#13965E",
     gray: "#D9D9D9",
     black: "#000",
+    blue: "#325AA4",
+    white: "#fff",
   },
 };

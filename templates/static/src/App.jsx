e7b96c63900397eebe7438ee168b2cdf2ddb942e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-10 17:03:28 -0300

    add theme in ThemeProvider

--- templates/static/src/App.jsx
+++ templates/static/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <div>
-      <h1>My React App</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>My React App</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 export default App;
---
+++ templates/static/src/theme/theme.js
@@ -0,0 +1,5 @@
+export default {
+  colors: {
+    green: "#13965E",
+  },
+};

7ec171cfe2d7dbb5df0c44ddee0c24d0eb9e6f8e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2022-10-10 14:06:04 -0300

    change extensios of js to jsx,and fix webpack to resolve extensios jsx

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -2,25 +2,28 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "../public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html"
-        })
-    ]
-}
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, "../public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx"],
+        },
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
